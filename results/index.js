45c3c42610e10ba4285bfc2f98b31401b9a32ed3
Caner Atici <caneratici.cs@gmail.com>
2022-04-14 21:28:07 +0300

    node project initialized

---
+++ results/index.js
@@ -0,0 +1,41 @@
+// NodeCount,PathLength,SimCnt,
+// CompileTime,KeyGenerationTime,
+// EncryptionTime,ExecutionTime,
+// DecryptionTime,ReferenceExecutionTime,
+// Mse
+
+const fs = require('fs')
+const csv = require('csv-parser')
+
+console.log('Reading data from result.csv')
+
+const data = []
+const processedData = {}
+
+fs.createReadStream('results.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+
+        const dataPoint = {
+            nodeCount: parseInt(row.NodeCount),
+            pathLength: parseInt(row.PathLength),
+            simCnt: parseInt(row.SimCnt),
+            compileTime: parseFloat(row.CompileTime),
+            keyGenerationTime: parseFloat(row.KeyGenerationTime),
+            encryptionTime: parseFloat(row.EncryptionTime),
+            executionTime: parseFloat(row.ExecutionTime),
+            decryptionTime: parseFloat(row.DecryptionTime),
+            referenceExecutionTime: parseFloat(row.ReferenceExecutionTime),
+            mse: parseFloat(row.Mse)
+        }
+
+        processedData[row.NodeCount] = processedData[row.NodeCount] || []
+        processedData[row.NodeCount].push(dataPoint)
+
+        data.push(dataPoint)
+    })
+    .on('end', () => {
+        console.log('Read all data')
+        console.table(data)
+        // console.log(processedData['40'])
+    });
