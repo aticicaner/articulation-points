d2451bb8c588d027313327785448909590db222e
Caner Atici <caneratici.cs@gmail.com>
2022-04-15 16:59:39 +0300

    plot correction

--- results/plotGraphs.js
+++ results/plotGraphs.js
@@ -6,3 +6,33 @@ const summaryJson = fs.readFileSync('summary.json', 'utf8')
 const summary = JSON.parse(summaryJson)
 const groups = Object.keys(summary)
 
+// use d3 to create a bar chart
+const barChart = d3.select('body')
+    .append('svg')
+    .attr('width', 1024)
+    .attr('height', 768)
+    .selectAll('rect')
+    .data(groups)
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * 100)
+    .attr('y', (d, i) => 768 - summary[d])
+    .attr('width', 100)
+    .attr('height', (d, i) => summary[d])
+    .attr('fill', 'blue')
+
+// render
+barChart.enter().append('text')
+    .attr('x', (d, i) => i * 100 + 50)
+    .attr('y', (d, i) => 768 - summary[d] + 20)
+    .attr('text-anchor', 'middle')
+    .text(d => d)
+
+// save pngs
+barChart.enter().append('svg:image')
+    .attr('xlink:href', (d, i) => `${d}.png`)
+    .attr('x', (d, i) => i * 100)
+    .attr('y', (d, i) => 768 - summary[d] - 100)
+    .attr('width', 100)
+    .attr('height', 100)
+
